9481c6798a1c7e9b83317381bb7015eb1429e1e3
Mohi-th <mohithmohith880@gmail.com>
2026-02-25 00:40:03 +0530

    removed horizontal scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
+import "./App.css"
 
 export default function App() {
   return(
-  <div className="h-screen w-screen">
+  <div className="myDiv min-h-screen w-screen overflow-hidden">
     <Home />
   </div> )
 }
